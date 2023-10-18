583928402af7ab2bbf7f42f94bef612f39ec4d09
ErickFireworks <erick@fireworks.studio>
2023-10-17 18:49:14 -0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Sitio web en construcción</h1>
+    </main>
+  );
+}
