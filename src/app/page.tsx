a774e5db40961a34876f04f5001ad406abea4722
ErickFireworks <erick@fireworks.studio>
2023-10-18 14:54:08 -0600

    Add font robot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Login } from "../components/Login";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Sitio web en construcción</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="font-semibold text-6xl/[80%] text-white">Login </h1>
+      <hr />
+      {/* <Login /> */}
     </main>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <>
+      <input type="text" />
+    </>
+  );
+};
